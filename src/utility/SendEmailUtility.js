ec428ab2b9d670eb2a7a1078476022d0c5e2198f
TanvirOvi786 <tanvirahmedovi.main@gmail.com>
2023-02-10 19:05:59 +0600

    TOP

--- src/utility/SendEmailUtility.js
+++ src/utility/SendEmailUtility.js
@@ -3,12 +3,13 @@ var nodemailer = require('nodemailer');
 const SendEmailUtility = async (EmailTo, EmailText, EmailSubject) => {
 
     let transporter = nodemailer.createTransport({
-        host: 'mail.teamrabbil.com',
-        port: 25,
+        service: 'smtp.ionos.com',
+        host: 'mail.tanvirahnmedovi.com',
+        port: 587,
         secure: false,
         auth: {
-            user: "info@teamrabbil.com",
-            pass: '~sR4[bhaC[Qs'
+            user: "info@tanvirahnmedovi.com",
+            pass: 'tanvir786ovi'
         }, tls: {
             rejectUnauthorized: false
         },
@@ -16,7 +17,7 @@ const SendEmailUtility = async (EmailTo, EmailText, EmailSubject) => {
 
 
     let mailOptions = {
-        from: 'Task Manager MERN <info@teamrabbil.com>',
+        from: 'Task Manager MERN <info@tanvirahnmedovi.com>',
         to: EmailTo,
         subject: EmailSubject,
         text: EmailText
